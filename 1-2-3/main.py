1c605bf83d8b7f60c57faac8b0ec17286cb4ea13
BHowland-FoodEnthusiast <brandon.howland@vtsd.com>
2024-11-15 13:06:09 -0500

    Started 122

--- 1-2-3/main.py
+++ 1-2-3/main.py
@@ -2,6 +2,7 @@
 import turtle as trtl
 
 #-----setup-----
+letter = "A"
 apple_image = "apple.gif" # Store the file name of your shape
 pear_image = "pear.gif"
 wn = trtl.Screen()
@@ -11,21 +12,32 @@ wn.addshape(pear_image)
 apple = trtl.Turtle()
 wn.bgpic("background.gif")
 pear = trtl.Turtle()
+letter_draw = trtl.Turtle()
+letter_draw.penup()
+letter_draw.goto(200, 200)
+letter_draw.pendown()
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
 def draw_apple(active_apple):
   active_apple.shape(apple_image)
+  draw_letter()
   wn.update()
 
-def fall_apple(active_apple):
-  active_apple.setheading(270)
-  active_apple.forward(150)
+def draw_letter():
+
+  letter_draw.color("white")
+  letter_draw.write("A", font=("Arial", 74, "bold"))
+
+def fall_apple():
+  apple.setheading(270)
+  apple.forward(150)
 
 def draw_pear(active_pear):
   active_pear.shape(pear_image)
   wn.update()
 #-----function calls-----
+wn.listen()
+wn.onkeypress(fall_apple, letter)
 draw_apple(apple)
-fall_apple(apple)
 draw_pear(pear)
 wn.mainloop()
